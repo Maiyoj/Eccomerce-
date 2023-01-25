6f0aad7dec25f8577cfc49b24539e08c6dffa111
Maiyoj <valentinemaiyo1995@gmail.com>
2023-01-25 22:17:43 +0300

    create routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// import ProductsDetails from './pages/ProductsDetails';
+import { Fragment } from 'react';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return(
+  <Fragment>
+    <Routes>
+     <Route exact path="/" element={<Home />} />
+     {/* <Route exact path="/ProductsDetails" element={<ProductsDetails />} /> */}
+     </Routes>
+  </Fragment> 
+  )
 };
 
 export default App;
