f5b121f5128c1f96f17878f7ca7d7c4efd6cb8a2
Maiyoj <valentinemaiyo1995@gmail.com>
2023-01-29 13:42:11 +0300

    [add] items in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,9 +4,28 @@ import { ProductContext } from '../contexts/ProductContext';
 
 const Home = () => {
   const {products} = useContext(ProductContext)
-  //console.log(products);
-  
-  return <div>Homepage</div>;
+  const FilteredProducts = products.filter((item)=>{
+    return(item.category === "men's clothing" || item.category==="women's clothing")
+  })
+ 
+  return(
+   <div>
+    <section className='py-16'>
+      <div className='container mx-auto'>
+        <div className='grid'>
+        {FilteredProducts.map((product)=>{
+          return(
+            <div className='bg-pink-200 w-full h-[300px] mb-4' id={product.id}>
+              {product.title}
+            </div>
+          )
+
+        })}
+      </div>
+      </div>
+    </section>
+
+    </div>);
 };
 
 export default Home;
