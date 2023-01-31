5f6785e0a7aa50717528eda86d68d0b8094cb658
Maiyoj <valentinemaiyo1995@gmail.com>
2023-01-31 20:17:44 +0300

    update product page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import {Bsplus,BsEyeFill} from 'react-icons/bs'
+import {BsPlus,BsEyeFill} from 'react-icons/bs'
 
 
 const Product = ({product}) => {
@@ -12,11 +12,19 @@ const Product = ({product}) => {
     <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
         <div className='w-full h-full flex justify-center items-center'>
           <div className='w-[200px] mx-auto flex justify-center items-center'>
+             <div className=' absolute top-0 right-0 bg-red-500'>
+              <button>
+                <div className='flex justify-center items-center text-white w-12 h-12'>
+                  <BsPlus className='text-3xl' />
+                </div>
+
+              </button>
+            </div>
           <img className='max-h-[160px] group-hover:scale-110 transition duration-300' src={image} alt=''/>
           </div>
         </div> 
     </div>
-
+    <div>2</div>
   </div>
   )
 };
