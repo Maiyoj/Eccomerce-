5f1e3afa535fbdc2e4cd84b115d25e151cc86a8f
Maiyoj <valentinemaiyo1995@gmail.com>
2023-02-03 16:04:20 +0300

    complete side navabar set of state

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useContext } from 'react';
+import { SidebarContext } from '../contexts/SidebarContext';
 
 const Header = () => {
+  const {isOpen, setIsOpen} = useContext(SidebarContext);
   return (
   <>
   <div>Header</div>
-  <div>Open/CloseSideBar</div>
+ <div onClick={()=>setIsOpen(!isOpen)}>Open/CloseSideBar</div> 
   </>
   );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,14 +2,15 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import {IoMdArrowForward} from 'react-icons/io';
 import {FiTrash2} from 'react-icons/fi';
-import CartItem from '../components/CartItem'
-import { SidebarContext} from '../contexts/SidebarContext'
+import CartItem from '../components/CartItem';
+import { SidebarContext} from '../contexts/SidebarContext';
+
 
 const Sidebar = () => {
   const{isOpen, handleClose} = useContext(SidebarContext);
   return (
   <>
-  <div className={`${ isOpen? 'right-0': 'right-full'} w-full bg-white  fixed top-0 h-full shadow-2xl md:w-[35vm] xl:max-w-[30vm] trasition-all duration-3--
+  <div className={`${ isOpen? 'right-0' : '-right-full'} w-full bg-white  fixed top-0 h-full shadow-2xl md:w-[35vw] xl:max-w-[30vw] trasition-all duration-300
   z-20 px-4 lg:px-[35px]`}>Sidebar</div>
   </>
   );
