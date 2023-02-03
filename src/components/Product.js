d31b071437660668b69a1cb096f2e2eb715efbed
Maiyoj <valentinemaiyo1995@gmail.com>
2023-02-03 15:06:08 +0300

    update sidebar

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,7 +6,6 @@ import {BsPlus,BsEyeFill} from 'react-icons/bs'
 
 const Product = ({product}) => {
   const {id, image,category, price,title} = product;
-
   return (
   <div>
     <div className='border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition'>
