cd0ca7b5f26813f9baeb81ff69751b8a6a6b687b
Maiyoj <valentinemaiyo1995@gmail.com>
2023-02-03 15:32:29 +0300

    hide sidebar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+  <>
+  <div>Header</div>
+  <div>Open/CloseSideBar</div>
+  </>
+  );
 };
 
 export default Header;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,7 +7,12 @@ import { SidebarContext} from '../contexts/SidebarContext'
 
 const Sidebar = () => {
   const{isOpen, handleClose} = useContext(SidebarContext);
-  return <div>Sidebar</div>;
+  return (
+  <>
+  <div className={`${ isOpen? 'right-0': 'right-full'} w-full bg-white  fixed top-0 h-full shadow-2xl md:w-[35vm] xl:max-w-[30vm] trasition-all duration-3--
+  z-20 px-4 lg:px-[35px]`}>Sidebar</div>
+  </>
+  );
 };
 
 export default Sidebar;
