a86548cb36b471bcc07755b66396a7b19364fb6a
Maiyoj <valentinemaiyo1995@gmail.com>
2023-02-08 16:22:49 +0300

    complete styling cart item

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -5,7 +5,7 @@ import {IoMdClose, IoMdAdd, IoMdRemove} from 'react-icons/io';
 const CartItem = ({item}) => {
   const {id, image, title,price, amount} = item;
   return (
-      <div className='flex'>
+      <div className='flex gap-x-4 py-2  lg:px-6 border-b border-grey-200 w-full font-light text-gray-500'>
           <div className='w-full min-h-[150px] flex items-center gap-x-4'>
             <Link to={`/product/${id}`}>
             <img className='max-w-[80px]'  src={image} alt=""/></Link>
@@ -18,17 +18,20 @@ const CartItem = ({item}) => {
         <IoMdClose className='text-grey hover:text-red-500 transition'/>
         </div>
         </div>
-        <div className='bg-pink-200 flex gap-x-2 h-[36px] text-sm'>
-          <div className=''>
-          <div><IoMdRemove/></div>
-          <div>{amount}</div> 
-          <div><IoMdAdd/></div>
-          <div>Item Price</div>
-          <div>Final  Price</div>
+        <div className='flex gap-x-2 h-[36px] text-sm'>
+          <div className='flex flex-1 max-w-[100px]  items-center h-full border text-primary font-medium'>
+          <div className='flex-1 flex justify-center items-center cursor-pointer'>
+            <IoMdRemove/>
           </div>
-
+          <div className='h-full flex  justify-center items-center px-2'>{amount}</div> 
+          <div className='flex-1 h-full flex justify-center items-center cursor-pointer'>
+            <IoMdAdd/>
+            </div>
+          </div>
+          <div className='flex-1 flex items-center justify-around'>${price}</div>
+          <div className='flex-1 flex justify-end items-center text-primary font-medium'>{`$ ${parseFloat(price * amount).toFixed(2)}`}</div>
+          </div>
         </div>
-      </div>
       </div>
       </div>
     )
