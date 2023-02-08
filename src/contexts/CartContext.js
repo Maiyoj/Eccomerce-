c7241ed20d9d3712b8b5700ba2b0e6aeca057461
Maiyoj <valentinemaiyo1995@gmail.com>
2023-02-08 11:22:56 +0300

    fixed a bug in cart context

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { createContext, useEffect, useState } from 'react';
+import Product from '../components/Product';
 
 export const CartContext = createContext()
 
 const CartProvider = ({children}) => {
+  const [cart, setCart] = useState([])
   
- const addToCart = (id)=>{
-  console.log(`Add ${id} to Cart`)
- }
+ const addToCart = (product, id)=>{
+  const newItem = {...product, amount: 1}
+  const cartItem = cart.find(item=>{
+    return (item.id === id);
+  });
+  //if cart item exist
+  if (cartItem) {
+    const newCart = [...cart].map((item)=>{
+      if (item.id ===id ) {
+        return ({...item, amout: cartItem.amount + 1});
+      }
+      else {
+        return item;
+      }
+    });
+    setCart(newCart);
+  }
+  else setCart([...cart, newItem]);
+ };
+ console.log(cart)
 
   return (
     <div>
