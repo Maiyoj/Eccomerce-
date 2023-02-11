a17785069d4874dd01536654e78985b4a66ceae1
Maiyoj <valentinemaiyo1995@gmail.com>
2023-02-11 11:58:23 +0300

    complete hero page

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -20,7 +20,7 @@ const Sidebar = () => {
   <div className='text-sm uppercase font-semibold'>Shopping Bag(0)</div>
   <div className='cursor-pointer w-8 h-8  flex justify-center items-center' onClick={handleClose}><IoMdArrowForward className='text-2xl'/></div>
    </div>
-   <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto 
+   <div className='flex flex-col gap-y-2  overflow-y-auto 
    overflow-x-hidden border-b'>{cart.map((item)=>{
     return(
       <div><CartItem key={item.id} item={item}/></div>
@@ -33,8 +33,11 @@ const Sidebar = () => {
         <span className='mr-2'>Total:</span>${parseFloat(total).toFixed(2)}
       </div>
       <div onClick={clearCart} className='cursor-pointer py-4  bg-red-500 text-white h-12 w-12 flex justify-center items-center text-xl'>
-        <FiTrash2/></div>
+        <FiTrash2/>
+      </div>
     </div>
+    <Link to={'/'} className='bg-gray-200 flex p-4 justify-center items-center text-primary w-full font-medium'>View Cart</Link>
+    <Link className='bg-primary flex p-4 justify-center items-center text-white w-full font-medium'>Checkout</Link>
    </div>
   </div>
   </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useContext } from 'react';
 import { ProductContext } from '../contexts/ProductContext';
+import Hero from '../components/Hero';
 import Product from '../components/Product';
 import { SidebarContext } from '../contexts/SidebarContext';
+import blackwoman from '../img/blackwoman.webp'
+
 
 const Home = () => {
  
@@ -11,10 +14,10 @@ const Home = () => {
     return(item.category === "jewelery" || item.category==="women's clothing")
   })
 
-  console.log(products)
  
   return(
    <div>
+    <Hero/>
     <section className='py-16'>
       <div className='container mx-auto'>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[30px]
