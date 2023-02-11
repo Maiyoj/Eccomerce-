2e3ea829b7c1b6e961ca84d1cd296825fa386f64
Maiyoj <valentinemaiyo1995@gmail.com>
2023-02-11 12:44:54 +0300

    update product pages

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext';
+import { ProductContext } from '../contexts/ProductContext';
+import { useParams } from 'react-router-dom';
 
 const ProductDetails = () => {
-  return <div>Product Details Page</div>;
+  const { id } = useParams();
+  const {products} = useContext(ProductContext);
+  const{addToCart} = useContext(CartContext);
+  const product = products.find((item)=>{
+    return item.id === parseInt(id);
+  });
+  if(!product){
+    return(
+      <section className='h-screen flex justify-center items-center'>
+        Loading...
+      </section>
+    );
+  }
+ const {title,price,description,image } = product
+  return (
+ <section className='pt-32 pb-12 lg:py-32 h-screen'>
+  <div className='container  mx-auto'>
+    <div>Image</div>
+    <div>Text</div>
+
+  </div>
+ </section>
+  )
 };
 
 export default ProductDetails;
